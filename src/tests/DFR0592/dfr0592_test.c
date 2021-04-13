b684520d2753d6c6e9eb14751347714067321a7a
Simão Amorim <simao.amorim@fe.up.pt>
2021-04-13 11:22:02 +0100

    Fix indentation

--- src/tests/DFR0592/dfr0592_test.c
+++ src/tests/DFR0592/dfr0592_test.c
@@ -3,20 +3,20 @@
 #include <errno.h>
 
 int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
-        return 1;
-    }
-    const struct dfr_board *b = board_init(1, 0x10);
-    if (NULL == b) {
-	fprintf(stderr, "board_init(%d, %d) failed\n", 1, 16);
-	return errno;
-    }
-    printf("fd = %d and addr = %d\n", b->i2c_fd, b->addr);
-    if (0 > board_close(b)) {
-	perror("board_close()");
-	return errno;
-    }
-    free((void *) b);
-    return 0;
+	if(argc != 1) {
+		printf("%s takes no arguments.\n", argv[0]);
+		return 1;
+	}
+	const struct dfr_board *b = board_init(1, 0x10);
+	if (NULL == b) {
+		fprintf(stderr, "board_init(%d, %d) failed\n", 1, 16);
+		return errno;
+	}
+	printf("fd = %d and addr = %d\n", b->i2c_fd, b->addr);
+	if (0 > board_close(b)) {
+		perror("board_close()");
+		return errno;
+	}
+	free((void *) b);
+	return 0;
 }
