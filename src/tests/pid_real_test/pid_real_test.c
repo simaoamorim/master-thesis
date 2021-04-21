b459bf4f912109d310b2fd2b27534d2f7017bb2f
Simão Amorim <up201605618@edu.fe.up.pt>
2021-04-21 11:54:42 +0100

    Move parameters to command line arguments for faster PID tunning

--- src/tests/pid_real_test/pid_real_test.c
+++ src/tests/pid_real_test/pid_real_test.c
@@ -27,15 +27,21 @@ void sighandler(int signum)
 
 int main (int argc, char *argv[])
 {
+	if (argc < 6) {
+		errno = EINVAL;
+		err(EXIT_FAILURE, "Not enough arguments");
+	}
 	int lret;
+	int period;
+	sscanf(argv[1], "%d", &period);
 	struct sched_attr attr = {
 		.size = sizeof(struct sched_attr),
 		.sched_policy = SCHED_DEADLINE,
 		.sched_priority = 0,
 		.sched_flags = 0,
-		.sched_period = 1000000,
-		.sched_runtime = 1000000,
-		.sched_deadline = 1000000
+		.sched_period = period,
+		.sched_runtime = period,
+		.sched_deadline = period
 	};
 
 	if (SIG_ERR == signal(SIGINT, sighandler))
@@ -55,54 +61,58 @@ int main (int argc, char *argv[])
 	encoder_enable(board, 1);
 
 	struct pid_t pid = {
-		.command = 800.0,
+	//	.command = 500.0,
 		.feedback = 0.0,
 		.delta_t = 0.0,
 
 		.deadband = 1.0,
 
-		.p_gain = 0.15,
-		.i_gain = 0.12,
-		.d_gain = 0.02,
+	//	.p_gain = 0.185,
+	//	.i_gain = 0.070,
+	//	.d_gain = 0.000,
 
 		.max_error = 100000.0,
 		.max_i_error = 100000.0,
-		.max_d_error = 10000.0,
+		.max_d_error = 1000.0,
 		.max_output = 100.0,
 
-		.d_output = 0.0,
-
-		.error = 0.0,
 		.previous_error = 0.0,
 	};
+	sscanf(argv[2], "%lf", &(pid.command));
+	sscanf(argv[3], "%lf", &(pid.p_gain));
+	sscanf(argv[4], "%lf", &(pid.i_gain));
+	sscanf(argv[5], "%lf", &(pid.d_gain));
 
 	struct timespec prev_time = {0}, cur_time = {0};
-	int speed_fb;
-
+	int fb;
 	clock_gettime(CLOCK_MONOTONIC, &prev_time);
 	sched_yield();
 
 	while (keep_running) {
-		encoder_get_speed(board, 1, &speed_fb);
+		encoder_get_speed(board, 1, &fb);
+		pid.feedback = (double) fb;
+
 		clock_gettime(CLOCK_MONOTONIC, &cur_time);
-		pid.feedback = speed_fb;
-		pid.delta_t = (cur_time.tv_sec - prev_time.tv_sec) * 1000000000UL + \
-			(cur_time.tv_nsec - prev_time.tv_nsec);
-		pid.delta_t /= 1000000000;
+		pid.delta_t = (cur_time.tv_sec - prev_time.tv_sec) + \
+			(cur_time.tv_nsec - prev_time.tv_nsec) / 1000000000.0;
+
 		do_calcs(&pid);
-		motor_set_speed(board, 1, get_output(&pid));
+
+		motor_set_speed(board, 1, (float) get_output(&pid));
 		prev_time = cur_time;
-		printf("Feedback: %d RPM\n", speed_fb);
-		printf("delta_t: %f ns\n", pid.delta_t);
-		printf("Error: %f RPM\n", pid.error);
-		printf("ErrorI: %f RPM\n", pid.i_error);
-		printf("ErrorD: %f RPM\n", pid.d_error);
-		printf("PrevError: %f\n", pid.previous_error);
-		printf("Output: %f %%\n", pid.output);
-		printf("OutputP: %f\n", pid.p_output);
-		printf("OutputI: %f\n", pid.i_output);
-		printf("OutputD: %f\n", pid.d_output);
-		puts("");
+
+		printf("\e[1;1H\e[2J");		// Clear screen
+		printf("Feedback:  %3.1f RPM\n", pid.feedback);
+		printf("delta_t:   %3.9f s\n", pid.delta_t);
+		printf("Error:     %3.6f RPM\n", pid.error);
+		printf("ErrorI:    %3.6f RPM\n", pid.i_error);
+		printf("ErrorD:    %3.6f RPM\n", pid.d_error);
+		printf("PrevError: %3.6f\n", pid.previous_error);
+		printf("Output:    %3.6f %%\n", pid.output);
+		printf("OutputP:   %3.6f\n", pid.p_output);
+		printf("OutputI:   %3.6f\n", pid.i_output);
+		printf("OutputD:   %3.6f\n", pid.d_output);
+
 		sched_yield();
 	}
 
