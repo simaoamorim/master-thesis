bce9ee0839336a4e8ef9fde1f7778d9e78bb67e5
Simão Amorim <up201605618@edu.fe.up.pt>
2021-04-27 15:10:25 +0100

    Request the lines as inputs

--- src/tests/encoder/encoder.c
+++ src/tests/encoder/encoder.c
@@ -7,9 +7,13 @@ int encoder_init (struct encoder *e, int gpiochip, int pin_a, int pin_b)
 		return -1;
 	e->a_line = gpiod_chip_get_line(e->chip_handle, pin_a);
 	e->b_line = gpiod_chip_get_line(e->chip_handle, pin_b);
+	if (NULL == e->a_line || NULL == e->b_line)
+		return -1;
 	e->stage = -1;
 	e->count = 0;
-	if (NULL == e->a_line || NULL == e->b_line)
+	if (-1 == gpiod_line_request_input(e->a_line, CONSUMER_NAME))
+		return -1;
+	if (-1 == gpiod_line_request_input(e->b_line, CONSUMER_NAME))
 		return -1;
 	return 0;
 }
