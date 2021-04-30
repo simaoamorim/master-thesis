8133eb0cf6bb1c1925b56a204e2d1087ca5baa6a
Simão Amorim <up201605618@edu.fe.up.pt>
2021-04-30 14:51:33 +0100

    Add mutex to the task

--- src/tests/encoder/encoder_task.c
+++ src/tests/encoder/encoder_task.c
@@ -1,6 +1,7 @@
 #include "encoder_task.h"
 
 int keep_running = 1;
+pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void sighandler (int signum)
 {
@@ -18,13 +19,16 @@ void * encoder_task (void *args)
 	if (-1 == sched_setscheduler(0, SCHED_FIFO, &sched_params)) {
 		pthread_exit((void *) -1);
 	}
-	struct encoder *enc;
-	enc = (struct encoder *) args;
+	struct encoder *enc = (struct encoder *) args;
+	//pthread_mutex_init(counter_mutex, NULL);
 	while (keep_running) {
 		encoder_read_values(enc);
 		encoder_decode_stage(enc);
+		pthread_mutex_lock(&counter_mutex);
 		encoder_update_counter(enc);
+		pthread_mutex_unlock(&counter_mutex);
 		usleep(150);
 	}
+	pthread_mutex_destroy(&counter_mutex);
 	pthread_exit((void *) 0);
 }
