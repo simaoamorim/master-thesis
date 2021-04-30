ae55c5a932b4264a4e83ca3cbd5be135db15758e
Simão Amorim <up201605618@edu.fe.up.pt>
2021-04-30 14:28:31 +0100

    Remove debug prints

--- src/tests/encoder/encoder_task.c
+++ src/tests/encoder/encoder_task.c
@@ -14,18 +14,12 @@ void * encoder_task (void *args)
 	if (SIG_ERR == signal(SIGINT, sighandler)) {
 		pthread_exit((void *) -1);
 	}
-	puts("Thread: signal OK");
 	struct encoder *enc;
 	enc = (struct encoder *) args;
-	puts("Thread: entering loop");
 	while (keep_running) {
-		puts("Thread: read inputs");
 		encoder_read_values(enc);
-		puts("Thread: decode stage");
 		encoder_decode_stage(enc);
-		puts("Thread: update counter");
 		encoder_update_counter(enc);
 	}
-	puts("Thread: terminating");
 	pthread_exit((void *) 0);
 }
