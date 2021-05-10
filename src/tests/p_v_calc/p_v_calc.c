48c02cb77cee76ebb6c0f611e7a0fa9f281bc017
Simão Amorim <up201605618@edu.fe.up.pt>
2021-05-10 11:25:48 +0100

    Prevent division by zero error

--- src/tests/p_v_calc/p_v_calc.c
+++ src/tests/p_v_calc/p_v_calc.c
@@ -17,6 +17,9 @@ double calc_velocity (long enc_count, double delta_t)
 {
 	static double prev_revs = 0.0;
 
+	if (0 == encoder_ppr || 0.0 == gearbox_ratio)
+		return 0;
+
 	double revs = (double) enc_count / encoder_ppr;
 	double motor_w = (revs - prev_revs) / delta_t;
 	double output_w = motor_w / gearbox_ratio * 60.0;
