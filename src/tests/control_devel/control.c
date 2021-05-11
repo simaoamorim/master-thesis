a580520699d2ebef252be69c0ad0027c07ae0c85
Simão Amorim <up201605618@edu.fe.up.pt>
2021-05-11 15:57:29 +0100

    Make the control application use the new velocity calculation thread

--- src/tests/control_devel/control.c
+++ src/tests/control_devel/control.c
@@ -29,24 +29,24 @@ void sighandler (int signum)
 int main (int argc, char *argv[])
 {
 	int retval = 0;
-	struct dfr_board *dfr_board = NULL;
+	struct dfr_board dfr_board = NEW_DFR_BOARD;
 	struct timespec first_time, prev_time, cur_time;
 	FILE *debug_file = NULL;
 	int iter = 0;
 	struct encoder_task encoder_struct = {0};
 	struct pid_t pid_s = NEW_PID_T;
+	struct p_v_task_s pv_task_s = NEW_P_V_TASK_S;
 	double tstamp;
-	long encoder_count = 0;
-	double output_velocity = 0.0;
-	pthread_t thread_id;
+	pthread_t encoder_thread_id = -1;
+	pthread_t p_v_thread_id = -1;
 	int control_period;
-	unsigned int inbuf_size = 80;
+	size_t inbuf_size = 80;
 	char *inbuf = calloc(sizeof(char), inbuf_size);
 	struct pollfd fds = {.fd = fileno(stdin), .events = POLLIN};
 	int lret;
 
 	// Check argument count
-	if (argc != 7 && argc != 8) {
+	if (argc != 10 && argc != 11) {
 		if (argc != 1)
 			fprintf(stderr, "Wrong usage\n\n");
 		print_help(argv);
@@ -59,16 +59,16 @@ int main (int argc, char *argv[])
 		FAIL("Failed to create a realtime task");
 
 	// Open connection with DFR0592 board
-	dfr_board = (struct dfr_board *) board_init(1, 0x10);
-	if (NULL == dfr_board)
+	lret = board_init(&dfr_board, 1, 0x10);
+	if (0 != lret)
 		FAIL("board_init(1, 0x10) failed");
-	if (0 != board_set_mode(dfr_board, DC))
+	if (0 != board_set_mode(&dfr_board, DC))
 		FAIL("board_set_mode(DC) failed");
-	set_pwm_frequency(dfr_board, 5000);
+	set_pwm_frequency(&dfr_board, 5000);
 	for (int i = 1; i < 3; i++) {
-		if (0 != encoder_disable(dfr_board, i))
+		if (0 != encoder_disable(&dfr_board, i))
 			FAIL("encoder_disable() failed");
-		if (0 != motor_stop(dfr_board, i))
+		if (0 != motor_stop(&dfr_board, i))
 			FAIL("motor_stop() failed");
 	}
 
@@ -79,13 +79,17 @@ int main (int argc, char *argv[])
 	sscanf(argv[4], "%lf", &pid_s.deadband);
 	sscanf(argv[5], "%d", &control_period);
 	sscanf(argv[6], "%lf", &pid_s.command);
+	sscanf(argv[7], "%ld", &pv_task_s.period);
+	sscanf(argv[8], "%ld", &pv_task_s.encoder_ppr);
+	sscanf(argv[9], "%lf", &pv_task_s.gearbox_ratio);
 
 	// Initializer encoder interface
 	if (-1 == encoder_init(&encoder_struct.encoder, 0, 17, 18))
 		FAIL("Failed to initialize encoder GPIO");
 
 	// Initialize encoder task thread
-	pthread_create(&thread_id, NULL, encoder_task, &encoder_struct);
+	pthread_create(&encoder_thread_id, NULL, encoder_task, &encoder_struct);
+	pthread_create(&p_v_thread_id, NULL, p_v_task, &pv_task_s);
 
 	if (SIG_ERR == signal(SIGINT, sighandler)) {
 		perror("signal(SIGINT, sighandler) failed");
@@ -93,13 +97,13 @@ int main (int argc, char *argv[])
 		goto end;
 	}
 
-	if (argc == 8) {
-		debug_file = init_pid_debug(&pid_s, argv[7]);
+	if (argc == 11) {
+		debug_file = init_pid_debug(&pid_s, argv[10]);
 		if (NULL != debug_file) {
 			debug_append_iteration(&pid_s, debug_file, iter, 0.0);
 		} else {
 			char string[100];
-			sprintf(string, "Failed to open debug file \"%s\": ", argv[7]);
+			sprintf(string, "Failed to open debug file \"%s\": ", argv[10]);
 			perror(string);
 			puts("Continuing without debug output");
 		}
@@ -107,6 +111,7 @@ int main (int argc, char *argv[])
 
 	printf("> ");
 
+	p_v_enable_task(&pv_task_s);
 	clock_gettime(CLOCK_MONOTONIC, &first_time);
 	prev_time = first_time;
 	usleep(control_period);
@@ -116,9 +121,9 @@ int main (int argc, char *argv[])
 
 		// Acquire inputs (including needed calculations)
 		pid_s.delta_t = delta(prev_time, cur_time);
-		encoder_count = encoder_task_get_count(&encoder_struct);
-		calc_velocity(encoder_count, pid_s.delta_t, &output_velocity);
-		pid_s.feedback = output_velocity;
+
+		//calc_velocity(encoder_count, pid_s.delta_t, &output_velocity);
+		pid_s.feedback = p_v_get_velocity(&pv_task_s);
 		// Check stdin for new command value
 		lret = poll(&fds, 1, 0);
 		if (0 < lret) {
@@ -136,7 +141,7 @@ int main (int argc, char *argv[])
 		do_calcs(&pid_s);
 
 		// Update outputs
-		motor_set_speed(dfr_board, 1, (float) get_output(&pid_s));
+		motor_set_speed(&dfr_board, 1, (float) get_output(&pid_s));
 
 		// Update memories
 		prev_time = cur_time;
@@ -154,16 +159,18 @@ int main (int argc, char *argv[])
 	printf("Exited main loop\n");
 
 end:
-	if (NULL != dfr_board) {
+	if (-1 != dfr_board.i2c_fd) {
 		printf("Stopping motor...");
-		motor_stop(dfr_board, 1);
-		free((void *) dfr_board);
+		motor_stop(&dfr_board, 1);
 		printf("OK\n");
 	}
-	printf("Stopping auxiliary thread...");
+	//printf("Stopping auxiliary thread...");
 	encoder_task_stop();
-	pthread_join(thread_id, (void *) &retval);
-	printf("OK\n");
+	p_v_task_stop();
+	if (-1 != encoder_thread_id)
+		pthread_join(encoder_thread_id, (void *) &retval);
+	if (-1 != p_v_thread_id)
+		pthread_join(p_v_thread_id, (void *) &retval);
 
 	return retval;
 }
@@ -171,14 +178,18 @@ end:
 void print_help (char *argv[])
 {
 	printf("Usage:\n");
-	printf("\t%s p_gain i_gain d_gain deadband period command [debug_filename]\n", argv[0]);
+	printf("  %s p_gain i_gain d_gain deadband period "\
+		"command p_v_period encoder_ppr gbox_ratio [filename]\n", argv[0]);
 	printf("\n");
 	printf("Arguments:\n");
-	printf("\tp_gain:\t\tProportional gain\n");
-	printf("\ti_gain:\t\tIntegral gain\n");
-	printf("\td_gain:\t\tDerivative gain\n");
-	printf("\tdeadband:\tDeadband value\n");
-	printf("\tperiod:\tControl period\n");
-	printf("\tcommand:\tCommand value\n");
-	printf("\tdebug_filename:\tFile name to output debug into [Optional]\n");
+	printf("  p_gain:      Proportional gain\n");
+	printf("  i_gain:      Integral gain\n");
+	printf("  d_gain:      Derivative gain\n");
+	printf("  deadband:    Deadband value\n");
+	printf("  period:      Control period\n");
+	printf("  command:     Command value\n");
+	printf("  p_v_period:  Period to calculate pos and vel\n");
+	printf("  encoder_ppr: Motor encoder PPR\n");
+	printf("  gbox_ratio:  Motor gearbox ratio\n");
+	printf("  filename:    File name to output debug into [Optional]\n");
 }
