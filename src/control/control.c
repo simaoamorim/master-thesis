54e409357bab7dc8cd902fad87a732f3cfc14680
Simão Amorim <up201605618@edu.fe.up.pt>
2021-05-12 12:13:34 +0100

    Initialization code for control task

--- src/control/control.c
+++ src/control/control.c
@@ -1,3 +1,31 @@
 #include "control.h"
 
+void * control_task (void *arg)
+{
+	struct control_s *cs = (struct control_s *) arg;
+/*
+	struct sched_param sp = {.sched_priority = cs->priority};
+	if (0 != sched_setscheduler(0, SCHED_FIFO, &sp))
+		goto fail;
+*/
+	struct timespec prev_time, cur_time;
 
+	clock_gettime(CLOCK_MONOTONIC, &prev_time);
+
+	do {
+		// Control loop here
+		usleep(cs->period);
+		clock_gettime(CLOCK_MONOTONIC, &cur_time);
+
+		prev_time = cur_time;
+	} while (cs->keep_running);
+
+	pthread_exit((void *) 0);
+fail:
+	pthread_exit((void *) -1);
+}
+
+void control_task_stop (struct control_s *cs)
+{
+	cs->keep_running = false;
+}
--- src/control/control.h
+++ src/control/control.h
@@ -1,8 +1,32 @@
 #ifndef CONTROL_H_
 #define CONTROL_H_
 
+#define	NEW_CONTROL_S {\
+	.pid_pos = 0,\
+	.pid_vel = 0,\
+	.period = 1000,\
+	.priority = 11,\
+	.enabled = false,\
+	.keep_running = true,\
+}
+
+#include <pthread.h>
+#include <sched.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <pid.h>
 
+struct control_s {
+	struct pid_s *pid_pos;
+	struct pid_s *pid_vel;
+	long period;
+	long priority;
+	bool enabled;
+	bool keep_running;
+};
 
+void * control_task (void *arg);
+
+void control_task_stop (struct control_s *cs);
 
 #endif
