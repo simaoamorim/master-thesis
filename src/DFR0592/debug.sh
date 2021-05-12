57f47b4f7ba0ab84ed8e5077bd10e3083d2d7f36
Simão Amorim <up201605618@edu.fe.up.pt>
2021-05-12 11:14:40 +0100

    Make tests copy for final code

---
+++ src/DFR0592/debug.sh
@@ -0,0 +1,2 @@
+valgrind --leak-check=summary --undef-value-errors=no $1\
+	./dfr0592_test
---
+++ src/encoder/encoder.c
@@ -0,0 +1,87 @@
+#include "encoder.h"
+
+int encoder_init (struct encoder *e, int gpiochip, int pin_a, int pin_b)
+{
+	e->chip_handle = gpiod_chip_open_by_number(gpiochip);
+	if (NULL == e->chip_handle)
+		return -1;
+	e->a_line = gpiod_chip_get_line(e->chip_handle, pin_a);
+	e->b_line = gpiod_chip_get_line(e->chip_handle, pin_b);
+	if (NULL == e->a_line || NULL == e->b_line)
+		return -1;
+	e->stage = -1;
+	e->new_stage = -1;
+	e->count = 0;
+	e->values[0] = 0;
+	e->values[1] = 0;
+	e->inputs = (struct gpiod_line_bulk *) malloc(sizeof(*e->inputs));
+	gpiod_line_bulk_init(e->inputs);
+	gpiod_line_bulk_add(e->inputs, e->a_line);
+	gpiod_line_bulk_add(e->inputs, e->b_line);
+	gpiod_line_request_bulk_input(e->inputs, CONSUMER_NAME);
+	return 0;
+}
+
+void encoder_read_values (struct encoder *e)
+{
+	gpiod_line_get_value_bulk(e->inputs, e->values);
+}
+
+void encoder_decode_stage (struct encoder *e)
+{
+	switch (e->values[0]) {
+		case 0: {
+			if (e->values[1])
+				e->new_stage = 3;
+			else
+				e->new_stage = 0;
+			break;
+		}
+		case 1: {
+			if (e->values[1])
+				e->new_stage = 2;
+			else
+				e->new_stage = 1;
+			break;
+		}
+		default:
+			break;
+	}
+}
+
+void encoder_update_counter (struct encoder *e)
+{
+	if (4 > e->stage) {
+		int diff = (int) (e->new_stage - e->stage);
+		if (diff > -3 && diff < 3)
+			e->count += diff;
+		else if (-3 == diff)
+			e->count++;
+		else if (3 == diff)
+			e->count--;
+	}
+	e->stage = e->new_stage;
+}
+
+int encoder_end (struct encoder *e)
+{
+	if (NULL != e->inputs) {
+		gpiod_line_release_bulk(e->inputs);
+		free(e->inputs);
+	} else if (NULL != e->a_line && NULL != e->b_line) {
+		gpiod_line_release(e->a_line);
+		gpiod_line_release(e->b_line);
+	}
+	e->inputs = NULL;
+	e->a_line = NULL;
+	e->b_line = NULL;
+	if (NULL != e->chip_handle)
+		gpiod_chip_close(e->chip_handle);
+	e->chip_handle = NULL;
+	return 0;
+}
+
+double apply_scale (long count, double scale)
+{
+	return (double) count / -scale;
+}
