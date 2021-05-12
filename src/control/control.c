cccd396d1b1c8067e126e4178f8c92390c41e948
Simão Amorim <up201605618@edu.fe.up.pt>
2021-05-12 12:20:06 +0100

    Add check for not null pid structure

--- src/control/control.c
+++ src/control/control.c
@@ -1,10 +1,16 @@
 #include "control.h"
 
+#define	delta(t1, t2) (t2.tv_sec - t1.tv_sec) + ((t2.tv_nsec - t1.tv_nsec) / 1000000000.0)
+
 void * control_task (void *arg)
 {
 	struct control_s *cs = (struct control_s *) arg;
-
 	struct timespec prev_time, cur_time;
+
+	if (cs->pid_pos != NULL)
+		puts("Using position PID");
+	if (cs->pid_vel != NULL)
+		puts("Using velocity PID");
 
 	clock_gettime(CLOCK_MONOTONIC, &prev_time);
 
